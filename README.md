6e63f76279e0205d17d92c70035ba0355baab4ac
frank201113 <1831328181@qq.com>
2023-07-16 21:31:56 +0800

    zentvicons

---
+++ README.md
@@ -0,0 +1,12 @@
+Vue3 SVG icon components integrated from [`zenticons`](https://github.com/zent-contrib/zenticons)
+
+can be imported like a component. try it now 🎉
+
+
+``` js
+import { LeftCircle } from 'zentvicons'
+
+<template>
+  <LeftCircle />
+</template>
+```
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Icon, IconConfigProvider } from './vicons-utils'
+import { Up, Youzan } from "../es";
+</script>
+
+<template>
+  <Icon color="red" size="48">
+    <Up />
+  </Icon>
+  <IconConfigProvider color="red" size="48">
+    <Icon>
+      <Up />
+    </Icon>
+  </IconConfigProvider>
+  <Icon color="red" size="48">
+    <Youzan />
+  </Icon>
+</template>
+
+<style scoped>
+</style>
